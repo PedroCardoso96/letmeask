ca4b55a9cbc52522be4580d47e037272c329e35c
Pedro Cardoso <pedrohfsc96@gmail.com>
2022-04-30 11:07:01 -0300

    rotas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+import { firebase, auth } from '../services/firebase'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 export function Home() {
+    
+    const navigate = useNavigate();
+    
+    function handleCreateRoom(){
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+            navigate('/rooms/new')
+        })
+
+
+
+    }
+
+
     return (
         <div id='page-auth'>
             <aside>
@@ -15,7 +33,7 @@ export function Home() {
             <main>
                 <div className='main-content'>
                     <img src={logoImg} alt="Letmeask" />
-                    <button className='create-room'>
+                    <button onClick={handleCreateRoom} className='create-room'>
                         <img src={googleIconImg} alt="Logo do Google" />
                         Crie sua sala com o Google
                     </button>
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -19,6 +19,9 @@ const firebaseConfig = {
 
   firebase.initializeApp(firebaseConfig);
 
-  export const auth = firebase.auth();
+  const auth = firebase.auth();
 //   export const database = firebase.database();
 //   export const firestore = firebase.firestore();
+
+export {firebase, auth}
+
